ebbe9e288dd804613a131fc01b9ceade37bbacaf
nicolamozzachiodi <nicola_mozzachiodi@icloud.com>
2018-12-02 13:18:27 +0100

    Update Site 021218

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    var nav = $('.navigation');
+    var scrollHeight = (($('.image img').outerHeight()) / 2) + $('.navigation').outerHeight() + 5;
+    scrollNav();
+    // Animation button on hero slider
+    $('.mouse img').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.about').offset().top - scrollHeight
+        }, 500);
+    });
+    // Animation navbar 
+    $(window).scroll(function() {
+        scrollNav();
+    });
+    function scrollNav() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            nav.addClass("newNav");
+        } else {
+            nav.removeClass("newNav");
+        }
+    }
+});
