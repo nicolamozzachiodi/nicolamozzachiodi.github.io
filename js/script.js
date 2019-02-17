d655c551d2c5bda0097ef9a4d44c9cdb21cd0349
nicolamozzachiodi <nicola_mozzachiodi@icloud.com>
2019-02-17 21:47:00 +0100

    Add menu mobile to site

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
-$(document).ready(function () {
-    var nav = $('.navigation');
-    var scrollHeight = (($('.image img').outerHeight()) / 2) + $('.navigation').outerHeight() + 5;
-    scrollNav();
-    // Animation button on hero slider
-    $('.mouse img').click(function () {
-        $('html, body').animate({
-            scrollTop: $('.about').offset().top - scrollHeight
-        }, 500);
-    });
-    // Animation navbar 
-    $(window).scroll(function() {
-        scrollNav();
-    });
-    function scrollNav() {
+var nav = $('.navigation');
+var scrollHeight = (($('.image img').outerHeight()) / 2) + $('.navigation').outerHeight() + 5;
+
+function scrollNav() {
+    if( $(window).width() > 768 ) {
         var scroll = $(window).scrollTop();
         if (scroll > 0) {
             nav.addClass("newNav");
@@ -20,11 +10,42 @@ $(document).ready(function () {
             nav.removeClass("newNav");
         }
     }
-    function startSlider() {
-        $('.slider').slick({
-            lazyLoad: 'ondemand',
-            slidesToShow: 2,
-            slidesToScroll: 1
-        });
+}
+
+// Animation button on hero slider
+function scrollArrow() {
+    $('.mouse img').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.about').offset().top - scrollHeight
+        }, 500);
+    });
+}
+
+function startSlider() {
+    $('.slider').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+}
+function navigation() {
+    $(".navBar").toggleClass("openNav");
+    var spanHamburger = $(".ham").toArray();
+    for(var i = 0; i < spanHamburger.length; i++) {
+        var element = spanHamburger[i]; 
+        if(i == 1) {
+            $(element).toggleClass("navOpenLeft");
+        } else {
+            $(element).toggleClass("navOpenRight");
+        } 
     }
+}
+
+$(document).ready(function() {
+    // Animation navbar 
+    $(".hamburger").on("click", navigation);
+    scrollNav();
+    $(window).scroll(scrollNav);
+    startSlider();
+    scrollArrow();
 });
