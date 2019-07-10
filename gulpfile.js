7e192377e276d74314cc402082393d2ecbc5c927
nicolamozzachiodi <nmozzachiodi@alpenite.com>
2019-07-10 21:14:11 +0200

    install gulpp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
-
-gulp.task('default', function(){
-  console.log('Gulp è un task manager coi fiocchi!');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('default', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
-
-gulp.task('default', function(){
-
-  var input  = 'src/**/*.css';
-  var output = 'build';
-  return gulp.src( input )
-   .pipe(gulp.dest( output ));
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
